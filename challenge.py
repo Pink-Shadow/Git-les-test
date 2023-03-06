f095956272acbbe89e41a36d089bbe348ee479dc
Jasper <jaspervdpost@gmail.com>
2023-03-06 14:11:49 +0100

    changed challenge.py file

--- challenge.py
+++ challenge.py
@@ -1,23 +1,9 @@
 #This file is here to let you practice with merge errors.
 
-def print_in_between(num, range):
-    print(f"nu bij: {num} *= {range}")
-
-def calc_factorial(num):
-    res = 1
-    for number in range(2, num+1):
-        print_in_between(res, number)
-        res *= number
-    return res
-
-def useless_function(acht):
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    acht *= 1532
-    acht //= 81
-    acht += 38
-    return letters[acht%letters.__len__()]
+def merge_conflict():
+    ''' This function is used to demonstrate merge conflicts'''
+    print("deze text moet je aanpassen")
 
 
 if __name__ == "__main__":
-    print(f"\nresult: {calc_factorial(10)}\n")
-    print(useless_function(11))
+    merge_conflict()
